6b760ef671e9fd392ef7002e438946ec64f4164b
neykov.m@gmail.com <neykov.m@gmail.com>
2019-05-14 13:10:55 +0300

    class structure, simple pojo, db connection

---
+++ project/src/main/java/com/example/project/models/Product.java
@@ -0,0 +1,51 @@
+package com.example.project.models;
+
+public class Product {
+    private Long id;
+    private String name;
+    private String description;
+    private byte[] image;
+    private double price;
+
+    public Product() {
+    }
+
+    public Product(Long id, String name, String description, double price) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
---
+++ project/src/main/java/com/example/project/repositories/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.example.project.repositories;
+
+import com.example.project.models.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
